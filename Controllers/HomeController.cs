9203a7e1448387691e75496dc31b1b159fcf3308
sakbar <syed.akbar@pubdef.state.mn.us>
2020-06-08 17:29:02 -0500

    initial checkin

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using graph_office.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Web;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using graph_office.Graph;
+using graph_office.Alerts;
+
+namespace graph_office.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private readonly ILogger<HomeController> _logger;
+
+
+        // dependency injection
+        public HomeController(            
+            ILogger<HomeController> logger)
+        {            
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [AllowAnonymous]
+        public IActionResult ErrorWithMessage(string message, string debug)
+        {
+            return View("Index").WithError(message, debug);
+        }
+    }
+    
+}
---
+++ Graph/GraphConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace graph_office.Graph
+{
+    public static class GraphConstants
+    {
+        // Defines the permission scopes used by the app
+        public readonly static string[] Scopes =
+        {
+            "User.Read",
+            "MailboxSettings.Read",
+            "Calendars.ReadWrite"
+        };
+    }
+    
+}
